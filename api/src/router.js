0bc69c8676337a032c3cd846ccd7937b11802d95
Amritha K <amrithakrishnan94@gmail.com>
2021-12-28 23:27:25 +0530

    Add upload routes and validator functions

---
+++ api/src/router.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const multer = require("multer");
+
+const filename = (request, file, callback) => {
+    callback(null, file.originalname);
+};
+
+const storage = multer.diskStorage({ destination: 'api/uploads/', filename });
+
+const fileFilter = (request, file, callback) => {
+    if (file.mimetype !== 'image/png') {
+        request.fileValidationError = "Wrong file type";
+        callback(null, false, new Error("Wrong file type"));
+    }
+    callback(null, true);
+}
+
+const upload = multer({ fileFilter, storage });
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if (request.fileValidationError) {
+        return response.status(400).json({ error: request.fileValidationError });
+    }
+    return response.status(201).json({ success: true });
+})
+
+module.exports = router;
