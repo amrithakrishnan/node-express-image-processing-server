43a82675437d890a9cfef5b373fe62ef33684869
Amritha K <amrithakrishnan94@gmail.com>
2021-12-29 00:15:29 +0530

    Add image processor functions

--- api/src/router.js
+++ api/src/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const multer = require("multer");
 const path = require('path');
-
+const imageProcessor = require('./imageProcessor');
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 const filename = (request, file, callback) => {
@@ -20,9 +20,15 @@ const fileFilter = (request, file, callback) => {
 
 const upload = multer({ fileFilter, storage });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if (request.fileValidationError) {
         return response.status(400).json({ error: request.fileValidationError });
+    }
+    
+    try {
+        await imageProcessor(request.file.filename);
+    } catch (err) {
+        console.log({err})
     }
     return response.status(201).json({ success: true });
 })
